814c74c14cd3e952042b3d0fb74114dd0ec06859
Gabriel Carrara <89613075+GabrielCarrara@users.noreply.github.com>
2021-08-30 12:13:36 -0400

    adicionei os bglz pia

---
+++ src/main/java/br/edu/ifmt/fundamentosbd/Inicializador/InicializadorDados.java
@@ -0,0 +1,76 @@
+package br.edu.ifmt.fundamentosbd.Inicializador;
+
+import br.edu.ifmt.fundamentosbd.modelo.Autor;
+import br.edu.ifmt.fundamentosbd.modelo.Livro;
+import br.edu.ifmt.fundamentosbd.repositorio.AutorRepositorio;
+import br.edu.ifmt.fundamentosbd.repositorio.LivroRepositorio;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InicializadorDados implements CommandLineRunner {
+
+    private final LivroRepositorio livroRepositorio;
+    private final AutorRepositorio autorRepositorio;
+
+    public InicializadorDados(LivroRepositorio livroRepositorio, AutorRepositorio autorRepositorio){
+        this.autorRepositorio = autorRepositorio;
+        this.livroRepositorio = livroRepositorio;
+
+    }
+    @Override
+    public void run(String... args) throws Exception {
+
+        // LIVRO 1
+        Livro livro1 = new Livro("Mano descobre a liberdade", "9788508147922");
+        Autor autor1 = new Autor("Gilberto", "Dimenstein");
+
+        livro1.getAutores().add(autor1);
+        autor1.getLivros().add(livro1);
+
+        autorRepositorio.save(autor1);
+        livroRepositorio.save(livro1);
+
+
+
+        //------------------------------------------------------------------------------------
+        // LIVRO 2
+        Livro livro2 = new Livro("A revolução dos bichos", "9780151002177");
+        Autor autor2 = new Autor("George", "Orwell");
+
+        livro2.getAutores().add(autor2);
+        autor2.getLivros().add(livro2);
+
+        autorRepositorio.save(autor2);
+        livroRepositorio.save(livro2);
+
+
+
+
+        //------------------------------------------------------------------------------------
+        // LIVRO 3
+        Livro livro3 = new Livro("A Moreninha", "9788508014729");
+        Autor autor3 = new Autor("Joaquim Manoel", "Macedo");
+
+        livro3.getAutores().add(autor3);
+        autor3.getLivros().add(livro3);
+
+        autorRepositorio.save(autor3);
+        livroRepositorio.save(livro3);
+
+
+
+        //------------------------------------------------------------------------------------
+
+        // MOSTRAR LIVROS
+        System.out.println("INICIALIZADOR DE DADOS OK...");
+        System.out.println("Numero de livros " +livroRepositorio.count());
+
+
+        //--------------------------------------------------------------------------------------
+
+
+
+
+    }
+}
--- src/main/java/br/edu/ifmt/fundamentosbd/modelo/Livro.java
+++ src/main/java/br/edu/ifmt/fundamentosbd/modelo/Livro.java
@@ -1,5 +1,80 @@
 package br.edu.ifmt.fundamentosbd.modelo;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
 public class Livro {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+
+        public String titulo;
+        public String isbn;
+
+        @ManyToMany
+        @JoinTable(name = "autores_livros", joinColumns = @JoinColumn(name = "livro_id"),
+                inverseJoinColumns = @JoinColumn(name = "autor_id"))
+        public Set<Autor> autores = new HashSet<>();
+
+        public Livro(){}
+
+        public Livro(String titulo, String isbn) {
+                this.titulo = titulo;
+                this.isbn = isbn;
+        }
+
+        public String getTitulo() {
+                return titulo;
+        }
+
+        public void setTitulo(String titulo) {
+                this.titulo = titulo;
+        }
+
+        public String getIsbn() {
+                return isbn;
+        }
+
+        public void setIsbn(String isbn) {
+                this.isbn = isbn;
+        }
+
+        public Set<Autor> getAutores() {
+                return autores;
+        }
+
+        public void setAutores(Set<Autor> autores) {
+                this.autores = autores;
+
+
+        }
+
+        @Override
+        public String toString() {
+                return "Livro{" +
+                        "id=" + id +
+                        ", titulo='" + titulo + '\'' +
+                        ", isbn='" + isbn + '\'' +
+                        ", autores=" + autores +
+                        '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Livro livro = (Livro) o;
+                return Objects.equals(id, livro.id);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(id);
+        }
+
 
 }
